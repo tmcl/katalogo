04d475ba26a04b2defce8c95e2475dc515a291df
Tristan McLeay <cassowary@fastmail.fm>
2015-09-06 11:05:40 +1000

    remove some disused code

--- frontend/src/test/grid.js
+++ frontend/src/test/grid.js
@@ -1,30 +1,11 @@
 /* @flow */
 
 import * as assert from "assert"
-import {LibraryFilter} from "../live/library filter"
 import {Catalogue} from "../live/library"
 import * as fs from 'fs'
 
-var LibraryFilterMock = new LibraryFilter( () => [
-		{
-			author: "Ivan Vazoy",
-			callnumber: "8-31 VAZ SUB",
-			pagecount: 459,
 			year: 1888,
-			publisher: "Bulgara Esperanto Asocio",
-			title: "Sub la JugAo",
-			translator: null
-		},
-		{
-			author: 'Martinus',
-			callnumber: '28 MAR',
-			pagecount: 96,
-			year: 1989,
-			publisher: "Folioj el la Bildlibro de Dio",
-			title: "Penlekosla brilo super la vivo",
-			translator: "G. Graversen"
 		}
-] )
 
 describe('library', () => {
 	var mock_library_json = fs.readFileSync( "../../../test-data/mea-katalogo-2.json", 'utf-8')
@@ -35,10 +16,6 @@ describe('library', () => {
 	})
 })
 
-describe('LibraryFilter', () => {
-	describe('#get_all_books()', () => {
-		it('should get two books', () => {
-			assert.equal(2, LibraryFilterMock.get_all_books().length)
 		})
 	})
 })
